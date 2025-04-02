164fd644c748446768e87748d91347e6237f97f7
Oleg Sherbakov <holdmann@yandex.ru>
2025-04-02 21:41:12 +0400

    all.

---
+++ app/settings.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Settings\Settings;
+use App\Application\Settings\SettingsInterface;
+use DI\ContainerBuilder;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder) {
+
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        SettingsInterface::class => function () {
+            return new Settings([
+                'displayErrorDetails' => true, // Should be set to false in production
+                'logError'            => false,
+                'logErrorDetails'     => false,
+                'logger' => [
+                    'name' => 'slim-app',
+                    'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                    'level' => Logger::DEBUG,
+                ],
+                'telegramBotToken' => '7959424182:AAH2t0vjgZRRlPHDkXp-95_j-LkRUzB3zTU'
+            ]);
+        }
+    ]);
+};
